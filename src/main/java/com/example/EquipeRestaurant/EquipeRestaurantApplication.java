92b3ceb4aacfa0d63b30492f73188819f2b1425e
mara <ms.vergnaud@gmail.com>
2024-02-21 11:09:37 +0100

    config

---
+++ src/main/java/com/example/EquipeRestaurant/EquipeRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.EquipeRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipeRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipeRestaurantApplication.class, args);
+	}
+
+}
