217acea803e5c7adae6333fc3f8ff2e75884e5a7
Melien Pierre <pimerors@gmail.com>
2024-02-21 16:58:19 +0100

    repository

--- src/main/java/com/example/EquipeRestaurant/entities/Reservation.java
+++ src/main/java/com/example/EquipeRestaurant/entities/Reservation.java
@@ -1,9 +1,20 @@
 package com.example.EquipeRestaurant.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@jakarta.persistence.Table(name = "reservations")
+@Data
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  int id;
+    @ManyToOne
     private Restaurant restaurant;
+    @ManyToOne
     private Client client;
+    @ManyToOne
     private  Table table;
 
 }
