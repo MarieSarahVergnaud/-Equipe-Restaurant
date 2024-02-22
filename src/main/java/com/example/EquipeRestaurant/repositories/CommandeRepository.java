d9c81f7d49b3586962c2f01c0ece1204292f49ad
Florian <florian.faneuil@gmail.com>
2024-02-22 16:32:39 +0100

    debut creation des methodes particulieres de commandes

--- src/main/java/com/example/EquipeRestaurant/repositories/CommandeRepository.java
+++ src/main/java/com/example/EquipeRestaurant/repositories/CommandeRepository.java
@@ -1,9 +1,14 @@
 package com.example.EquipeRestaurant.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.EquipeRestaurant.entities.Commande;
+import com.example.EquipeRestaurant.entities.Restaurant;
 
 public interface CommandeRepository extends CrudRepository<Commande, Integer> {
+	
+	List<Commande> findByTableEtatContainingAndTableRestaurantIdEquals(String etat, int id_restaurant);
 
 }
--- src/main/java/com/example/EquipeRestaurant/services/CommandeService.java
+++ src/main/java/com/example/EquipeRestaurant/services/CommandeService.java
@@ -1,9 +1,12 @@
 package com.example.EquipeRestaurant.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.EquipeRestaurant.entities.Commande;
+import com.example.EquipeRestaurant.entities.Restaurant;
 import com.example.EquipeRestaurant.repositories.CommandeRepository;
 
 @Service
@@ -24,6 +27,10 @@ public class CommandeService {
 	
 	public void delete(int id) {
 		dao.deleteById(id);
+	}
+	
+	public List<Commande> findByEtatEnPrepa(int id_restaurant){
+		return dao.findByTableEtatContainingAndTableRestaurantIdEquals("EN PREPA", id_restaurant);
 	}
 	
 }
