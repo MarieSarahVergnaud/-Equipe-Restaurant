3f0fd9780f37b7654125c936f2b5f0fb1ef0ec6c
mara <ms.vergnaud@gmail.com>
2024-02-22 10:36:35 +0100

    resolution problem Tables

--- src/main/java/com/example/EquipeRestaurant/entities/Reservation.java
+++ src/main/java/com/example/EquipeRestaurant/entities/Reservation.java
@@ -1,26 +1,26 @@
 package com.example.EquipeRestaurant.entities;
 
-
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.GenerationType;
 import lombok.Data;
 
 @Entity
 @jakarta.persistence.Table(name = "reservations")
 @Data
 public class Reservation {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  int id;
-    @ManyToOne
-    private Restaurant restaurant;
-    @ManyToOne
-    private Client client;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@ManyToOne
+	private Restaurant restaurant;
+	@ManyToOne
+	private Client client;
 
-    private Tables table;
+	// A CHANGER
+	@ManyToOne
+	private Tables table;
 
 }
