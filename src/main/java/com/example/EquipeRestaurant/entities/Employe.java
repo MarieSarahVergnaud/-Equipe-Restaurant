ed979e5cf26f70c414a2ca91214296d67f934f0e
mara <ms.vergnaud@gmail.com>
2024-02-23 14:50:15 +0100

    /protected avec en header le token de l'employe OK

--- src/main/java/com/example/EquipeRestaurant/entities/Employe.java
+++ src/main/java/com/example/EquipeRestaurant/entities/Employe.java
@@ -2,6 +2,8 @@ package com.example.EquipeRestaurant.entities;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,27 +14,32 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-@Entity @Table(name="employes")
+@Entity
+@Table(name = "employes")
 @Data
 public class Employe {
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-	
-	@Column(name="nom")
-    private String nom;
-	@Column(name="prenom")
-    private String prenom;
-	@Column(name="email")
-    private String email;
-	@Column(name="password")
-    private String password;
-	@Column(name="token")
-    private String token;
-	@Column(name="role")
-    private String role;
-	@Column(name="expiration_time")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "nom")
+	private String nom;
+	@Column(name = "prenom")
+	private String prenom;
+	@Column(name = "email")
+	private String email;
+	// Json Ignore pour ne pas renvoyer les password
+	@JsonIgnore
+	@Column(name = "password")
+	private String password;
+	@Column(name = "token")
+	private String token;
+	@Column(name = "role")
+	private String role;
+	@Column(name = "expiration_time")
 	private LocalDateTime expirationTime;
-	
-	@OneToOne @JoinColumn(name="id_restaurant")
-    private Restaurant restaurant;
+
+	@OneToOne
+	@JoinColumn(name = "id_restaurant")
+	private Restaurant restaurant;
 }
